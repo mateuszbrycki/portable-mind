4b39c0f7996b555793f9fee9a1a952173c8785d9
Mateusz Brycki <mateusz.brycki@outlook.com>
2015-09-27 18:03:06 +0200

    SYSTEM-011 ProjectController

---
+++ src/main/java/com/portablemind/project/ProjectController.java
@@ -0,0 +1,54 @@
+package com.portablemind.project;
+
+import com.portablemind.card.Card;
+import com.portablemind.card.service.CardService;
+import com.portablemind.cardCategory.CardCategory;
+import com.portablemind.cardCategory.service.CardCategoryService;
+import com.portablemind.project.service.ProjectService;
+import com.portablemind.user.UserUtilities;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Controller
+@RequestMapping(ProjectUrls.PROJECT)
+public class ProjectController {
+    private String viewPath = "controller/project/";
+
+    @Inject
+    ProjectService projectService;
+
+    @Inject
+    CardService cardService;
+
+    @Inject
+    CardCategoryService cardCategoryService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String get(@PathVariable("projectId") Integer projectId, ModelMap model) {
+        Integer userId = UserUtilities.getLoggedUserId();
+
+        Project project = projectService.findById(projectId);
+        model.addAttribute("project", project);
+
+        List<Card> cards = cardService.findAllProjectCards(projectId);
+        model.addAttribute("cards", cards);
+
+
+        List<Project> projects = projectService.findAllUserProjects(userId);
+        model.addAttribute("projects", projects);
+
+        List<CardCategory> cardCategories = cardCategoryService.findAllCardCategories();
+        model.addAttribute("cardCategories", cardCategories);
+
+        return this.viewPath + "list";
+
+    }
+
+
+}
