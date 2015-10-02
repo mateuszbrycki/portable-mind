c9f8a78de8d30c89e40ec88d29d53bead18fb40c
Mateusz Brycki <mateusz.brycki@outlook.com>
2015-10-02 21:08:56 +0200

    DATABASE-002 - new database dump (user roles, card categories)

--- database/portable_mind.sql
+++ database/portable_mind.sql
@@ -1,207 +1,269 @@
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Baza danych: `portable_mind`
+-- Database: `portable_mind`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `card`
+-- Table structure for table `card`
 --
+
 CREATE TABLE IF NOT EXISTS `card` (
-`id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `fk_user_id` int(255) NOT NULL,
   `fk_category_id` int(255) NOT NULL,
   `fk_project_id` int(255) NOT NULL,
+  `card_name` text,
   `audit_cd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `audit_md` timestamp NULL DEFAULT NULL,
-  `card_description` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+  `card_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `card_category`
+-- Table structure for table `card_category`
 --
+
 CREATE TABLE IF NOT EXISTS `card_category` (
-`id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `category_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `fk_user_id` int(255) NOT NULL,
   `audit_cd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `audit_md` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+  `audit_md` timestamp NULL DEFAULT NULL,
+  `category_icon` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Struktura tabeli dla tabeli `language`
+-- Dumping data for table `card_category`
 --
+
+INSERT INTO `card_category` (`id`, `category_name`, `fk_user_id`, `audit_cd`, `audit_md`, `category_icon`) VALUES
+(1, 'Linux command', 1, '2015-05-03 12:26:39', NULL, 'linux.png'),
+(2, 'SQL Query', 1, '2015-05-03 12:28:59', NULL, 'sql.png'),
+(3, 'Git command', 1, '2015-05-03 17:06:31', NULL, 'git.png'),
+(4, 'PHP Code', 1, '2015-05-03 17:10:22', NULL, 'php.png'),
+(5, 'Plain text', 1, '2015-08-12 14:44:47', NULL, 'text.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
+--
+
 CREATE TABLE IF NOT EXISTS `language` (
-`id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Struktura tabeli dla tabeli `project`
+-- Dumping data for table `language`
 --
+
+INSERT INTO `language` (`id`, `name`) VALUES
+(1, 'pl_PL'),
+(2, 'en_US');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
 CREATE TABLE IF NOT EXISTS `project` (
-`id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text,
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_polish_ci,
   `fk_user_id` int(255) NOT NULL,
   `audit_cd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `audit_md` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `translation`
+-- Table structure for table `translation`
 --
+
 CREATE TABLE IF NOT EXISTS `translation` (
-`id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   `fk_language_id` int(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
--- Struktura tabeli dla tabeli `user`
+-- Table structure for table `user`
 --
+
 CREATE TABLE IF NOT EXISTS `user` (
-`id` int(255) NOT NULL,
+  `id` int(255) NOT NULL,
   `fk_user_role` int(11) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `password` text NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `audit_cd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `audit_md` timestamp NULL DEFAULT NULL,
   `is_public` tinyint(1) NOT NULL,
   `is_enabled` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `user_role`
+-- Table structure for table `user_role`
 --
+
 CREATE TABLE IF NOT EXISTS `user_role` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `role` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
 --
--- Indeksy dla zrzutów tabel
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_MODERATOR'),
+(3, 'ROLE_USER');
+
+--
+-- Indexes for dumped tables
 --
 
 --
 -- Indexes for table `card`
 --
 ALTER TABLE `card`
- ADD PRIMARY KEY (`id`), ADD KEY `fk_category_id` (`fk_category_id`), ADD KEY `fk_project_id` (`fk_project_id`), ADD KEY `fk_user_id_card` (`fk_user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_category_id` (`fk_category_id`),
+  ADD KEY `fk_user_id_card` (`fk_user_id`),
+  ADD KEY `fk_project_id` (`fk_project_id`);
 
 --
 -- Indexes for table `card_category`
 --
 ALTER TABLE `card_category`
- ADD PRIMARY KEY (`id`), ADD KEY `fk_user_id_card_category` (`fk_user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id_card_category` (`fk_user_id`);
 
 --
 -- Indexes for table `language`
 --
 ALTER TABLE `language`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
- ADD PRIMARY KEY (`id`), ADD KEY `fk_user_id` (`fk_user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id` (`fk_user_id`);
 
 --
 -- Indexes for table `translation`
 --
 ALTER TABLE `translation`
- ADD PRIMARY KEY (`id`), ADD KEY `fk_language_id` (`fk_language_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_language_id` (`fk_language_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`id`), ADD KEY `fk_user_role` (`fk_user_role`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_role` (`fk_user_role`);
 
 --
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `card`
+-- AUTO_INCREMENT for table `card`
 --
 ALTER TABLE `card`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT dla tabeli `card_category`
+-- AUTO_INCREMENT for table `card_category`
 --
 ALTER TABLE `card_category`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT dla tabeli `language`
+-- AUTO_INCREMENT for table `language`
 --
 ALTER TABLE `language`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT dla tabeli `project`
+-- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT dla tabeli `translation`
+-- AUTO_INCREMENT for table `translation`
 --
 ALTER TABLE `translation`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT dla tabeli `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 --
--- AUTO_INCREMENT dla tabeli `user_role`
+-- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `card`
+-- Constraints for table `card`
 --
 ALTER TABLE `card`
-ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`fk_category_id`) REFERENCES `card_category` (`id`) ON DELETE CASCADE,
-ADD CONSTRAINT `fk_project_id` FOREIGN KEY (`fk_project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE,
-ADD CONSTRAINT `fk_user_id_card` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_category_id` FOREIGN KEY (`fk_category_id`) REFERENCES `card_category` (`id`),
+  ADD CONSTRAINT `fk_project_id` FOREIGN KEY (`fk_project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `fk_user_id_card` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`);
 
 --
--- Ograniczenia dla tabeli `card_category`
+-- Constraints for table `card_category`
 --
 ALTER TABLE `card_category`
-ADD CONSTRAINT `fk_user_id_card_category` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `fk_user_id_card_category` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`);
 
 --
--- Ograniczenia dla tabeli `project`
+-- Constraints for table `project`
 --
 ALTER TABLE `project`
-ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`);
 
 --
--- Ograniczenia dla tabeli `translation`
+-- Constraints for table `translation`
 --
 ALTER TABLE `translation`
-ADD CONSTRAINT `fk_language_id` FOREIGN KEY (`fk_language_id`) REFERENCES `language` (`id`);
+  ADD CONSTRAINT `fk_language_id` FOREIGN KEY (`fk_language_id`) REFERENCES `language` (`id`);
 
 --
--- Ograniczenia dla tabeli `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
-ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`fk_user_role`) REFERENCES `user_role` (`id`);
+  ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`fk_user_role`) REFERENCES `user_role` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
